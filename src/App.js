25236c58180cb4c65ed3496f64392c77351c3bf0
miciex <maciejmilewski222@gmail.com>
2022-11-01 20:50:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import './App.css'
-import MainPage from './pages/mainPage/MainPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ErrorPage from './pages/errorPage/ErrorPage'
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainPage />} path='/' />
-        <Route element={<MainPage />} path='/contact' />
-        <Route element={<MainPage />} path='/projects' />
-        <Route element={<MainPage />} path='/story' />
-        <Route element={<MainPage />} path='/settings' />
-        <Route element={<ErrorPage />} path='*' />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <div>Welcome to boilerplate with react 18 and tailwind 3!</div>;
 }
 
-export default App
+export default App;
--- src/consts.js
+++
@@ -1,1 +0,0 @@
-export const LINKS = ['/', '/contact', '/projects', '/story', '/settings']
